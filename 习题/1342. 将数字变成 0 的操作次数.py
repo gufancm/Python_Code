d99c6b87867e038967ece1291033fe00d4f5c5f1
gufan <117725545+gufancm@users.noreply.github.com>
2023-04-07 10:52:16 +0800

    Create 1342. 将数字变成 0 的操作次数.py

--- 习题/1342. 将数字变成 0 的操作次数.py
+++ 习题/1342. 将数字变成 0 的操作次数.py
@@ -1,11 +1,10 @@
 # 给你一个非负整数 num ，请你返回将它变成 0 所需要的步数。 如果当前数字是偶数，你需要把它除以 2 ；否则，减去 1 。
-class Solution:
-    def numberOfSteps(self, num: int) -> int:
-        count = 0
-        while (num != 0):
-            if num %2 == 0:
-                num /= 2
-            else:
-                num -= 1
-            count += 1
-        return count
+num = 10
+count = 0
+while (num != 0):
+    if num % 2 == 0:
+        num /= 2
+    else:
+        num -= 1
+    count += 1
+print(count)
